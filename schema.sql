609220eefcec7d8ebfadc88dae005dc3095483c2
bosshogg86 <jeffhogg86@gmail.com>
2020-09-30 13:31:54 -0700

    fixes schema INT

--- schema.sql
+++ schema.sql
@@ -10,14 +10,14 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR (255) NOT NULL,
     salary DECIMAL (10) NOT NULL,
-    department_id INT (4) NOT NULL,
+    department_id INT NOT NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR (255) NOT NULL,
     last_name VARCHAR (255) NOT NULL,
-    role_id INT (4) NOT NULL,
-    manager_id INT (4) NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
     PRIMARY KEY (id)
 );
