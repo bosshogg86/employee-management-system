62deae795d2b4a4eb6f2b6bf1b16c685e6fc26db
bosshogg86 <jeffhogg86@gmail.com>
2020-09-30 13:59:06 -0700

    adds class constructors

--- app.js
+++ app.js
@@ -3,3 +3,28 @@ const mysql = require("mysql");
 const cTable = require("console.table");
 const fs = require("fs");
 const path = require("path");
+const Employee = require("./lib/Employee");
+const Role = require("./lib/Role");
+const Department = require("./lib/Department");
+
+let connection;
+if (process.env.NODE_ENV === "production") {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  });
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "employeeDB",
+  });
+}
+connection.connect(err => {
+  if (err) throw err;
+  console.log("connected");
+  //   runSearch();
+});
