40adebac56fb58cd1e989eb423272ecf59cb431f
bosshogg86 <jeffhogg86@gmail.com>
2020-10-02 13:59:05 -0700

    updates schema and seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,30 +1,27 @@
--- insert into dept
---insert into roles----------------------------------------------------------------
--- insert into employees
--- then update employee manager
-INSERT INTO departments
-VALUES (1, 'Audio'),
-  (2, 'Video'),
-  (3, 'Camera'),
-  (4, 'Tape'),
-  (5, 'Production');
-INSERT INTO roles
-VALUES (1, 'A1', 100000, 1),
-  (2, 'A2', 80000, 1),
-  (3, 'V1', 120000, 2),
-  (4, 'Camera Operator', 100000, 3),
-  (5, 'EVS', 120000, 4),
-  (6, 'RO', 120000, 4),
-  (7, 'Director', 200000, 5),
-  (8, 'Producer', 250000, 5);
-INSERT INTO employees
-VALUES (1, 'James', 'Mileta', 4, 3),
-  (2, 'James', 'Duysen', 4, 3),
-  (3, 'Kyle', 'Farnham', 7, 8),
-  (4, 'Steve', 'Lowe', 5, 3),
-  (5, 'Raymond', 'Rodriguez', 6, 4),
-  (6, 'Blake', 'Bos', 1, 3),
-  (7, 'Sam', 'Jack', 2, 6),
-  (8, 'Laura', 'Johnson', 8, NULL),
-  (9, 'Brandon', 'Collins', 3, 3),
-  (10, 'Eric', 'Sanchagrin', 4, 3);
+INSERT INTO departments (name)
+VALUES ('Audio'),
+  ('Video'),
+  ('Camera'),
+  ('Tape'),
+  ('Production');
+INSERT INTO roles (title, salary, department_id)
+VALUES ('A1', 100000, 1),
+  ('A2', 80000, 1),
+  ('V1', 120000, 2),
+  ('Camera Operator', 100000, 3),
+  ('EVS', 120000, 4),
+  ('RO', 120000, 4),
+  ('Director', 200000, 5),
+  ('Producer', 250000, 5);
+INSERT INTO employees (firstName, lastName, role_id)
+VALUES ('James', 'Mileta', 4),
+  ('James', 'Duysen', 4),
+  ('Kyle', 'Farnham', 7),
+  ('Steve', 'Lowe', 5),
+  ('Raymond', 'Rodriguez', 6),
+  ('Blake', 'Bos', 1),
+  ('Sam', 'Jack', 2),
+  ('Laura', 'Johnson', 8),
+  ('Brandon', 'Collins', 3),
+  ('Eric', 'Sanchagrin', 4);
+-- update employee manager
