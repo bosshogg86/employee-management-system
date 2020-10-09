747b97ab3b627be2275eabb465cceba395a1788c
bosshogg86 <jeffhogg86@gmail.com>
2020-10-09 13:58:50 -0700

    adds, updates and deletes all

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const menu = () =>
       'View employees',
       'View employees by manager',
       'Add employee',
-      //'Update employee manager',
+      'Update employee manager',
       'Update employee role',
       'Delete employee',
       'View roles',
       'Add role',
-      //'Delete role',
+      'Delete role',
       'View departments',
       'Add department',
-      //'Delete department',
+      'Delete department',
       'Exit',
     ],
   });
@@ -39,8 +39,9 @@ const init = async () => {
       case 'Update employee role':
         updateEmployeeRole();
         break;
-      // case 'Update employee manager':
-      //   break;
+      case 'Update employee manager':
+        updateEmployeeManager();
+        break;
       case 'Delete employee':
         deleteEmployee();
         break;
@@ -50,16 +51,18 @@ const init = async () => {
       case 'Add role':
         addRole();
         break;
-      // case 'Delete role':
-      //   break;
+      case 'Delete role':
+        deleteRole();
+        break;
       case 'View departments':
         viewDepartments();
         break;
       case 'Add department':
         addDepartment();
         break;
-      // case 'Delete department':
-      //   break;
+      case 'Delete department':
+        deleteDept();
+        break;
       case 'Exit':
         exit();
         break;
@@ -67,20 +70,6 @@ const init = async () => {
   } catch (err) {
     console.log(err);
   }
-};
-
-const viewDepartments = async () => {
-  const res = await connection.query('SELECT * FROM departments');
-  console.table(res);
-  init();
-};
-
-const viewRoles = async () => {
-  const res = await connection.query(
-    'SELECT roles.id, title, salary, department_id, department FROM roles LEFT JOIN departments ON roles.department_id = departments.id'
-  );
-  console.table(res);
-  init();
 };
 
 const viewEmployees = async () => {
@@ -109,50 +98,6 @@ const viewEmployeesByManager = async () => {
     `SELECT employees.id, manager_id, firstName, lastName FROM employees LEFT JOIN roles ON manager_id = employees.id WHERE manager_id = ${byManager};`
   );
   console.table(res);
-  init();
-};
-
-const addDepartment = async () => {
-  const addRes = await inquirer.prompt({
-    type: 'input',
-    name: 'name',
-    message: 'What is the name of the new department?',
-  });
-  connection.query(
-    `INSERT INTO departments (department) VALUES ('${addRes.name}');`
-  );
-  console.table(addRes.name + ' department added');
-  init();
-};
-
-const addRole = async () => {
-  let departments = await connection.query('SELECT * FROM departments');
-  departments = departments.map(row => {
-    const department = { name: row.department, value: row.id };
-    return department;
-  });
-  const { title, salary, dept } = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the name of the role?',
-    },
-    {
-      type: 'input',
-      name: 'salary',
-      message: `What is the role's salary?`,
-    },
-    {
-      type: 'list',
-      name: 'dept',
-      message: `Which department is the role in?`,
-      choices: departments,
-    },
-  ]);
-  connection.query(
-    `INSERT INTO roles (title, salary, department_id) VALUES ('${title}', '${salary}', '${dept}');`
-  );
-  console.table(title + ' role added');
   init();
 };
 
@@ -204,15 +149,16 @@ const addEmployee = async () => {
 };
 
 const updateEmployeeRole = async () => {
-  const employees = await connection.query(
-    "SELECT employees.id AS id, CONCAT(`firstName`, ' ', `lastName`) AS name, title, roles.id AS role_id FROM employees LEFT JOIN roles ON roles.id = employees.role_id"
+  let employees = await connection.query(
+    "SELECT id, CONCAT(`firstName`, ' ', `lastName`) AS name FROM employees;"
   );
-  let employeesRes = employees.map(row => {
+  employees = employees.map(row => {
     const employee = { name: row.name, value: row.id };
     return employee;
   });
-  let roles = employees.map(row => {
-    const role = { name: row.title, value: row.role_id };
+  let roles = await connection.query('SELECT title, id FROM roles;');
+  roles = roles.map(row => {
+    const role = { name: row.title, value: row.id };
     return role;
   });
 
@@ -221,7 +167,7 @@ const updateEmployeeRole = async () => {
       type: 'list',
       name: 'updateEmp',
       message: `Which employee's role would you like to update?`,
-      choices: employeesRes,
+      choices: employees,
     },
     {
       type: 'list',
@@ -230,11 +176,40 @@ const updateEmployeeRole = async () => {
       choices: roles,
     },
   ]);
-
   connection.query(
     `UPDATE employees SET role_id=${updateRole} WHERE id=${updateEmp};`
   );
   console.table('Role updated');
+  init();
+};
+
+const updateEmployeeManager = async () => {
+  let employees = await connection.query(
+    "SELECT id, CONCAT(`firstName`, ' ', `lastName`) AS name FROM employees;"
+  );
+  employees = employees.map(row => {
+    const employee = { name: row.name, value: row.id };
+    return employee;
+  });
+
+  const { updateEmp, updateManager } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'updateEmp',
+      message: `Which employee's manager would you like to update?`,
+      choices: employees,
+    },
+    {
+      type: 'list',
+      name: 'updateManager',
+      message: `What is the employee's new manager'?`,
+      choices: employees,
+    },
+  ]);
+  connection.query(
+    `UPDATE employees SET manager_id=${updateManager} WHERE id=${updateEmp};`
+  );
+  console.table('Manager updated');
   init();
 };
 
@@ -249,11 +224,105 @@ const deleteEmployee = async () => {
   const { deleteEmp } = await inquirer.prompt({
     type: 'list',
     name: 'deleteEmp',
-    message: `Which employee's role would you like to delete?`,
+    message: `Which employee would you like to delete?`,
     choices: employees,
   });
   connection.query(`DELETE FROM employees WHERE id=${deleteEmp};`);
   console.log('Employee deleted');
+  init();
+};
+
+const viewRoles = async () => {
+  const res = await connection.query(
+    'SELECT roles.id, title, salary, department_id, department FROM roles LEFT JOIN departments ON roles.department_id = departments.id'
+  );
+  console.table(res);
+  init();
+};
+
+const addRole = async () => {
+  let departments = await connection.query('SELECT * FROM departments');
+  departments = departments.map(row => {
+    const department = { name: row.department, value: row.id };
+    return department;
+  });
+  const { title, salary, dept } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of the role?',
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: `What is the role's salary?`,
+    },
+    {
+      type: 'list',
+      name: 'dept',
+      message: `Which department is the role in?`,
+      choices: departments,
+    },
+  ]);
+  connection.query(
+    `INSERT INTO roles (title, salary, department_id) VALUES ('${title}', '${salary}', '${dept}');`
+  );
+  console.table(title + ' role added');
+  init();
+};
+
+const deleteRole = async () => {
+  let roles = await connection.query('SELECT id, title FROM roles');
+  roles = roles.map(row => {
+    const role = { name: row.title, value: row.id };
+    return role;
+  });
+  const { deleteRole } = await inquirer.prompt({
+    type: 'list',
+    name: 'deleteRole',
+    message: `Which role would you like to delete?`,
+    choices: roles,
+  });
+  connection.query(`DELETE FROM roles WHERE id=${deleteRole};`);
+  console.log('Role deleted');
+  init();
+};
+
+const viewDepartments = async () => {
+  const res = await connection.query('SELECT * FROM departments');
+  console.table(res);
+  init();
+};
+
+const addDepartment = async () => {
+  const addRes = await inquirer.prompt({
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of the new department?',
+  });
+  connection.query(
+    `INSERT INTO departments (department) VALUES ('${addRes.name}');`
+  );
+  console.table(addRes.name + ' department added');
+  init();
+};
+
+const deleteDept = async () => {
+  let departments = await connection.query(
+    'SELECT id, department FROM departments'
+  );
+  departments = departments.map(row => {
+    const department = { name: row.department, value: row.id };
+    return department;
+  });
+  const { deleteDept } = await inquirer.prompt({
+    type: 'list',
+    name: 'deleteDept',
+    message: `Which department would you like to delete?`,
+    choices: departments,
+  });
+  connection.query(`DELETE FROM departments WHERE id=${deleteDept};`);
+  console.log('Department deleted');
   init();
 };
 
